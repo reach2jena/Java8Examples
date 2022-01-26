68fc749254c57866bba2672c2ab2059f6c293d93
Rakesh Jena <reach2jena@gmail.com>
2022-01-26 22:07:00 +0530

    first commit

---
+++ src/com/saanvi/Person.java
@@ -0,0 +1,7 @@
+package com.saanvi;
+
+@
+public class Person {
+
+	
+}
---
+++ src/com/saanvi/Streams.java
@@ -0,0 +1,13 @@
+package com.saanvi;
+
+import java.util.List;
+
+public class Streams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Interview Preparations ------- >> ");
+	}
+
+	List<E>
+}
